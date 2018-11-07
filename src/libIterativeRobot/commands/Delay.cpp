cdbe096e6e8309408a4d016721be3f0793077961
ciherrera20 <ciherrera20@chaminet.org>
2018-11-07 09:08:02 -0800

    Added Delay command

---
+++ src/libIterativeRobot/commands/Delay.cpp
@@ -0,0 +1,35 @@
+#include "libIterativeRobot/commands/Delay.h"
+#include "libIterativeRobot/Robot.h"
+#include "Constants.h"
+
+Delay::Delay(int duration) {
+  this->priority = 1;
+  this->duration = duration;
+}
+
+bool Delay::canRun() {
+  return true; // This is the default value anyways, so this method can be removed
+}
+
+void Delay::initialize() {
+  // Perform any initialization steps for this command here, not in the
+  // constructor
+  startTime = pros::millis();
+}
+
+void Delay::execute() {
+  //printf("Moving claw to: %d\n", target);
+}
+
+bool Delay::isFinished() {
+    return pros::millis() - startTime >= duration;
+}
+
+void Delay::end() {
+  // Code that runs when isFinished() returns true.
+}
+
+void Delay::interrupted() {
+  // Code that runs when this command is interrupted by another one
+  // with a higher priority.
+}
