adbe1258ecbab28ada1f88e32aa507ac76d65e64
Alexander L. Edwards <ALEdwards21@chaminet.org>
2018-11-08 16:54:58 -0800

    Updated auton

--- src/libIterativeRobot/commands/AutonGroup1.cpp
+++ src/libIterativeRobot/commands/AutonGroup1.cpp
@@ -4,13 +4,14 @@
 
 AutonGroup1::AutonGroup1() {
   //addSequentialCommand(new MoveFlipperFor(5000, 127, false));
-  addSequentialCommand(new MoveBaseTo(3000, 3000));
+  addSequentialCommand(new MoveBaseTo(3300, 3300));
   //addSequentialCommand(new MoveFlipperFor(500, 0, false));
-  addSequentialCommand(new MoveBaseTo(-500, -500));
+  //addSequentialCommand(new MoveBaseTo(-300, -300));
   addSequentialCommand(new MoveBaseTo(800, -800));
-  addSequentialCommand(new MoveBaseTo(-300, 300));
-  addSequentialCommand(new MoveBaseTo(1200, 1800));
+  addSequentialCommand(new MoveBaseTo(1300, 1300));
   addParallelCommand(new MoveFlipperFor(1000, 127, false));
+  //addSequentialCommand(new MoveBaseTo(800, 800));
+  //addParallelCommand(new MoveFlipperFor(1000, 127, false));
   /*addSequentialCommand(new MoveBaseTo(-800, 800));
   addSequentialCommand(new MoveBaseTo(1000, 1000));
   addParallelCommand(new MoveFlipperFor(1000, -127, false));*/
